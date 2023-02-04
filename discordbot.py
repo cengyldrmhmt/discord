4f13aeecd4080f40fe95fc632382e02a72b072cd
cengyldrmhmt <122272954+cengyldrmhmt@users.noreply.github.com>
2023-02-05 02:19:31 +0300

    Add files via upload

---
+++ discordbot.py
@@ -0,0 +1,152 @@
+import discord
+import asyncio
+
+DCTOKEN ='<Token>'
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    image ='vip.gif'
+    #discord için
+    
+#     allText= ("VIP MEMBERSHIP PURCHASE WITH BUYMEACOFFEE IS ACTIVE AGAIN.\n\n"+
+#     "🤩  VIP MEMBERSHIP BENEFITS  🤩 \n\n"+
+#     "1️⃣ VIP MEMBERS CAN REQUEST. <https://badgirlsarchive.site/index.php?forums/4>\n\n"+
+#     "2️⃣ VIP MEMBERS GET DIRECT ACCESS TO MEGA FILES WITHOUT ADVERTISING (LINKVERTISE).\n\n"+
+#     "3️⃣ VIP MEMBERS CAN GIVE UNLIMITED ACCESS TO NEW ONLYFANS LEAK FILES FROM THE SPECIAL FORUM LINK. (THIS CONTENTS ARE NOT AVAILABLE ANYWHERE AND ARE EXCLUSIVE TO BADGIRLSARHIVE ONLY.)"+
+#     "<https://badgirlsarchive.site/index.php?forums/6/> (YOU CAN VIEW AS A MEMBER.)\n\n"+
+#     "3️⃣ VIP MEMBERS CAN GIVE UNLIMITED ACCESS TO THE CONTENT ON THE ONLYFANS VIP FORUM. ( IT CONTAINS 10000+ GIRLS AND 1000TB+ CONTENT.)\n"+
+#     "Click the link for vip content thread <https://badgirlsarchive.site/index.php?forums/6/>\n\n"+
+#     "4️⃣ YOU CAN JOIN THE SPECIALLY CREATED VIP TELEGRAM CHANNEL. ONLY MEGA FILES ARE SHARED ON THIS CHANNEL AND DOES NOT CONTAIN ANY ADVERTISING LINKS.\n\n"+
+#     "ONLY VIP 6 AND UNLIMITED VIP MEMBERS CAN JOIN THIS VIP TELEGRAM CHANNEL (YOU WILL NOT BE REMOVED AFTER JOINING)\n\n"+          
+#     "⭐️ TO BE A VIP MEMBER - > <https://badgirlsarchive.site/index.php?threads/588/>\n\n\n"+
+#     "VIP MEMBERSHIP PRICES\n\n"+
+#     "⭐️1 Month Subscription is Just $5.\n"+
+#     "⭐️3 Month Subscription is Just $14.\n"+
+#     "⭐️6 Month Subscription is Just $27.  𝗖𝗔𝗡 𝗝𝗢𝗜𝗡 𝗧𝗛𝗘 𝗩𝗜𝗣 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 𝗖𝗛𝗔𝗡𝗡𝗘𝗟🍑\n"+
+#     "⭐UNLIMITED MEMBERSHIP is ONLY $99  𝗖𝗔𝗡 𝗝𝗢𝗜𝗡 𝗧𝗛𝗘 𝗩𝗜𝗣 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 𝗖𝗛𝗔𝗡𝗡𝗘𝗟🍑 [RECOMMENDED]🔥\n\n\n"+
+#     "💲 You can pay with coinbase or buymeacoffee.\n\n"+
+#     "<https://badgirlsarchive.site/index.php?threads/588/>  (TO BE A VIP MEMBER)👑\n")
+    
+#     DCnewtelegramemoji = "NEW TELEGRAM CHANNEL(JOIN NOW!!!)\n👉👉 <https://t.me/+n_fYUQAJ7-w1MmRk>\n\n"
+#     dctitle = "🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱\n\n\n"+ex_title+"\n\n\n 🟥🟥MEGA PACK LINK : "
+#     dcmegalink = "<"+ex_megalink+">"
+#     dcvipLink = "<"+ex_viplink+">"
+#     dcvipMemberLink ="\n\n✔CLICK HERE --- BECOME VIP - > <https://badgirlsarchive.site/index.php?threads/588/>"
+#     dcviprequest="\n\n📝Request page (only for VIP users)-> <https://badgirlsarchive.site/index.php?forums/4/>"
+#     dcbuyvipmemcoffee="\n\n ☕TELEGRAM CHANNEL IS OPENED FOR UNLIMITED VIP MEMBERS.\n UNLIMITED MEMBERSHIP -> 💲99"
+# 
+#     allText = DCnewtelegramemoji+dctitle+dcmegalink+dcbuyvipmemcoffee+dcviprequest+vipText+dcvipLink+dcvipMemberLink+emojis
+# 
+#     allText_Access = dctitle+"<"+ex_vipmegalink+">"
+    
+    
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(1065371090215501924) #MAIN mega pack
+    await channel.send(allText + "@everyone" ,file = file)
+
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(917127603658321930) #makenzi
+    await channel.send(allText + " @everyone" ,file = file)
+
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(916425904715087872) #hannahowo
+    await channel.send(allText + " @everyone" ,file = file)
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(1010249043563249765) #hannahowo 2
+    await channel.send(allText + " @everyone" ,file = file)
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(999761825480593478) #corinna
+    await channel.send(allText + " @everyone" ,file = file)
+
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(941287632074309703) #heyimbee
+    await channel.send(allText + " @everyone" ,file = file)
+
+
+
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(1001955652018638930) #megnutt
+    await channel.send(allText + " @everyone" ,file = file)
+
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(916427112070643723) #Belle delphine
+    await channel.send(allText + " @everyone" ,file = file)
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(993841009559605298) #Sofiia gomez
+    await channel.send(allText + " @everyone" ,file = file)
+
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(961969275000729620) #CELEBRTY
+    await channel.send(allText + " @everyone" ,file = file)
+
+
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(961974049775423508) #TONİ STORM
+    await channel.send(allText + " @everyone" ,file = file)
+
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(962016971598225488) #twitch girls
+    await channel.send(allText + " @everyone" ,file = file)
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(962259389044105247) #tiktok girls
+    await channel.send(allText + " @everyone" ,file = file)
+
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(962314610445266994) #Tessa Flower
+    await channel.send(allText + " @everyone" ,file = file)
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(993875155275223060) #Cas Summer
+    await channel.send(allText + " @everyone" ,file = file)
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(993875205371994183) #Amouranth
+    await channel.send(allText + " @everyone" ,file = file)
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(1006677415935873075) #skybri
+    await channel.send(allText + " @everyone" ,file = file)
+
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(1007428097492144149) #THEBOYSPACK
+    await channel.send(allText + " @everyone" ,file = file)
+
+
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(1018175623795048509) #YENI DISCORD NORMAL yedek
+    await channel.send(allText + "\n @everyone\n" ,file = file)
+
+    # new discord
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(1021019054095478794) #YENI DISCORD NORMAL yedek badgirls
+    await channel.send(allText + "\n @everyone\n" ,file = file)
+    #miklay
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(1059548731000758382) #YENI DISCORD NORMAL  
+    await channel.send(allText + "\n @everyone\n" ,file = file)
+    
+    #Belle Delphine
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(1060251421821374504) #YENI DISCORD NORMAL  
+    await channel.send(allText + "\n @everyone \n" ,file = file)
+                                          
+    # new discord HEYIMBEE NEW CHANNEL
+    file = discord.File(image, filename = image)
+    channel = client.get_channel(1063857564246356049) #YENI DISCORD NORMAL 
+    await channel.send(allText + "\n @everyone\n" ,file = file)
+client.run(DCTOKEN)
